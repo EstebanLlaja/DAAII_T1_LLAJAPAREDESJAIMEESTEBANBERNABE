0a6ab894116cc32794e0af8593e64b518b470026
EstebanLlaja <jaime.esteban.bernabe@gmail.com>
2024-06-07 14:11:24 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE/DaaiiT1LlajaparedesjaimeestebanbernabeApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1LlajaparedesjaimeestebanbernabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1LlajaparedesjaimeestebanbernabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE/DaaiiT1LlajaparedesjaimeestebanbernabeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1LlajaparedesjaimeestebanbernabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
