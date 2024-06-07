7917b3d942bce5f3f39d5f53dfa2d104261958f5
EstebanLlaja <jaime.esteban.bernabe@gmail.com>
2024-06-07 16:49:24 -0500

    termino 2

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE/service/IUsuarioService.java
@@ -0,0 +1,16 @@
+package pe.edu.cibertec.DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE.service;
+
+
+
+import pe.edu.cibertec.DAAII_T1_LLAJAPAREDESJAIMEESTEBANBERNABE.model.bd.Usuario;
+
+import java.util.List;
+
+public interface IUsuarioService {
+
+    Usuario buscarUsuarioXNomUsuario(String nomusuario);
+    Usuario guardarUsuario(Usuario usuario);
+    void actualizarUsuario(Usuario usuario);
+    List<Usuario> listarUsuario();
+    Usuario buscarUsuarioXIdUsuario(Integer idusuario);
+}
